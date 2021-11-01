5cb78b7ebacd90fbd52f84a06f9c5dfd4d11090a
Oscar Andrés Castaño <89107253+Osicasta@users.noreply.github.com>
2021-10-31 22:27:16 -0500

    Java Actualización

--- Unidad_3.java
+++ Unidad_3.java
@@ -1,110 +1,96 @@
 import javax.swing.JOptionPane;
 
-public class Unidad_3{
+public class Unidad_3 {
     public static void main(String[] args) {
-        int opcion; 
-        do{
-        opcion=Integer.parseInt(JOptionPane.showInputDialog(
-        "Menú Principal\n\n"
-        +"1. Signos de Zodiaco\n"
-        +"2. Calcular edad de humano en años de perro\n"
-        +"3. Calcular número de días vividos\n"
-        +"4. Numerología\n" 
-        +"5. Salir"));
-        if(opcion==1)
-        {
-            szodiaco();
-        }
-        if(opcion==2)
-        {
-            eperro();
-        }
-        if(opcion==3)
-        {
-            diasV();
-        }
-        if(opcion==4)
-        {
-            numero();
-        }       
-        }while(opcion<6);
-        JOptionPane.showMessageDialog(null, "La opción ingresada es incorrecta");   
+        int opcion;
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Menú Principal\n\n" + "1. Signos de Zodiaco\n" + "2. Calcular edad de humano en años de perro\n"
+                            + "3. Calcular número de días vividos\n" + "4. Numerología\n" + "5. Salir"));
+            if (opcion == 1) {
+                szodiaco();
+            }
+            if (opcion == 2) {
+                eperro();
+            }
+            if (opcion == 3) {
+                diasV();
+            }
+            if (opcion == 4) {
+                numero();
+            }
+        } while (opcion < 6);
+        JOptionPane.showMessageDialog(null, "La opción ingresada es incorrecta");
     }
-    public static void szodiaco()
-    {
-        String mes; 
+
+    public static void szodiaco() {
+        String mes;
         int dia;
-        mes=JOptionPane.showInputDialog("Digite el mes de su nacimiento");
-        dia=Integer.parseInt(JOptionPane.showInputDialog("Digite el día de su nacimiento"));
-        if(mes.equalsIgnoreCase("Enero") && dia>=21 && dia<=31 || mes.equalsIgnoreCase("Febrero") && dia<=19)
-        {
+        mes = JOptionPane.showInputDialog("Digite el mes de su nacimiento");
+        dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el día de su nacimiento"));
+        if (mes.equalsIgnoreCase("Enero") && dia >= 21 && dia <= 31 || mes.equalsIgnoreCase("Febrero") && dia <= 19) {
             JOptionPane.showMessageDialog(null, "Su signo es: ACUARIO ");
         }
-        if(mes.equalsIgnoreCase("Febrero") && dia>=20 && dia<=28 || mes.equalsIgnoreCase("Marzo") && dia<=20)
-        {
+        if (mes.equalsIgnoreCase("Febrero") && dia >= 20 && dia <= 28 || mes.equalsIgnoreCase("Marzo") && dia <= 20) {
             JOptionPane.showMessageDialog(null, "Su signo es: PISCIS ");
         }
-        if(mes.equalsIgnoreCase("Marzo") && dia>=21 && dia<=31 || mes.equalsIgnoreCase("Abril") && dia<=20)
-        {
+        if (mes.equalsIgnoreCase("Marzo") && dia >= 21 && dia <= 31 || mes.equalsIgnoreCase("Abril") && dia <= 20) {
             JOptionPane.showMessageDialog(null, "Su signo es: ARIES ");
         }
-        if(mes.equalsIgnoreCase("Abril") && dia>=21 && dia<=30 || mes.equalsIgnoreCase("Mayo") && dia<=21)
-        {
+        if (mes.equalsIgnoreCase("Abril") && dia >= 21 && dia <= 30 || mes.equalsIgnoreCase("Mayo") && dia <= 21) {
             JOptionPane.showMessageDialog(null, "Su signo es: TAURO ");
         }
-        if(mes.equalsIgnoreCase("Mayo") && dia>=22 && dia<=31 || mes.equalsIgnoreCase("Junio") && dia<=21)
-        {
+        if (mes.equalsIgnoreCase("Mayo") && dia >= 22 && dia <= 31 || mes.equalsIgnoreCase("Junio") && dia <= 21) {
             JOptionPane.showMessageDialog(null, "Su signo es: GÉMINIS ");
         }
-        if(mes.equalsIgnoreCase("Junio") && dia>=22 && dia<=30 || mes.equalsIgnoreCase("Julio") && dia<=22)
-        {
+        if (mes.equalsIgnoreCase("Junio") && dia >= 22 && dia <= 30 || mes.equalsIgnoreCase("Julio") && dia <= 22) {
             JOptionPane.showMessageDialog(null, "Su signo es: CÁNCER ");
         }
-        if(mes.equalsIgnoreCase("Julio") && dia>=23 && dia<=31 || mes.equalsIgnoreCase("Agosto") && dia<=23)
-        {
+        if (mes.equalsIgnoreCase("Julio") && dia >= 23 && dia <= 31 || mes.equalsIgnoreCase("Agosto") && dia <= 23) {
             JOptionPane.showMessageDialog(null, "Su signo es: LEO ");
         }
-        if(mes.equalsIgnoreCase("Agosto") && dia>=24 && dia<=31 || mes.equalsIgnoreCase("Septiembre") && dia<=23)
-        {
+        if (mes.equalsIgnoreCase("Agosto") && dia >= 24 && dia <= 31
+                || mes.equalsIgnoreCase("Septiembre") && dia <= 23) {
             JOptionPane.showMessageDialog(null, "Su signo es: VIRGO ");
         }
-        if(mes.equalsIgnoreCase("Septiembre") && dia>=24 && dia<=30 || mes.equalsIgnoreCase("Octubre") && dia<=23)
-        {
+        if (mes.equalsIgnoreCase("Septiembre") && dia >= 24 && dia <= 30
+                || mes.equalsIgnoreCase("Octubre") && dia <= 23) {
             JOptionPane.showMessageDialog(null, "Su signo es: LIBRA ");
         }
-        if(mes.equalsIgnoreCase("Octubre") && dia>=24 && dia<=31 || mes.equalsIgnoreCase("Noviembre") && dia<=22)
-        {
+        if (mes.equalsIgnoreCase("Octubre") && dia >= 24 && dia <= 31
+                || mes.equalsIgnoreCase("Noviembre") && dia <= 22) {
             JOptionPane.showMessageDialog(null, "Su signo es: SCORPIO ");
         }
-        if(mes.equalsIgnoreCase("Noviembre") && dia>=23 && dia<=30 || mes.equalsIgnoreCase("Diciembre") && dia<=21)
-        {
+        if (mes.equalsIgnoreCase("Noviembre") && dia >= 23 && dia <= 30
+                || mes.equalsIgnoreCase("Diciembre") && dia <= 21) {
             JOptionPane.showMessageDialog(null, "Su signo es: SAGITARIO ");
         }
-        if(mes.equalsIgnoreCase("Diciembre") && dia>=22 && dia<=31 || mes.equalsIgnoreCase("Enero") && dia<=20)
-        {
+        if (mes.equalsIgnoreCase("Diciembre") && dia >= 22 && dia <= 31 || mes.equalsIgnoreCase("Enero") && dia <= 20) {
             JOptionPane.showMessageDialog(null, "Su signo es: CAPRICORNIO ");
         }
     }
-    public static void diasV()
-    {
 
+    public static void diasV() {
 
     }
-    public static void eperro()
-    {
-        double edad, edadper; 
-        edad=Double.parseDouble(JOptionPane.showInputDialog("Digite su edad"));
-        edadper=edad/7;
-        JOptionPane.showMessageDialog(null, "Usted tiene "+edadper+" años en edad de perro");
+
+    public static void eperro() {
+        double edad, edadper, perroedad;
+        edad = Double.parseDouble(JOptionPane.showInputDialog("Digite su edad"));
+        edadper = edad * 7;
+        perroedad = (Math.log(edad) * 16) + 31;
+        JOptionPane.showMessageDialog(null, "Usted tiene " + edadper + " años en edad de perro");
+        JOptionPane.showMessageDialog(null, "Usted tiene " + perroedad + " años en edad de perro con la nueva formula");
+
     }
-    public static void numero()
-    {
-        int diaN, num, n1, n2; 
-        diaN=Integer.parseInt(JOptionPane.showInputDialog("Digite su dia de nacimiento"));
-        if(diaN>9)
-        {
-            n1=diaN/10;
-            n2=
+
+    public static void numero() {
+        int diaN, num, n1, n2;
+        diaN = Integer.parseInt(JOptionPane.showInputDialog("Digite su dia de nacimiento"));
+        if (diaN > 9) {
+            n1 = diaN / 10;
+
         }
     }
+
 }
